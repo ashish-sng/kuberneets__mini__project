385484aebe1cc2ee7e7ab31394007c7b0becd8c6
sarthak24s <70680297+sarthak24s@users.noreply.github.com>
2023-05-04 12:33:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "react-alice-carousel/lib/alice-carousel.css";
-import CryptoContext from "./CryptoContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <CryptoContext>
-      <App />
-    </CryptoContext>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
